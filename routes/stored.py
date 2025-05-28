20afa15f0b6f5003696b9ffa4d6ceb30eab53d9d
Kliiyu <adri2007900@gmail.com>
2025-05-28 09:10:37 +0200

    added: new item stored endpoint

--- routes/stored.py
+++ routes/stored.py
@@ -9,4 +9,10 @@ async def get_items():
     stored = await db.stored.find().to_list(100)
     return fix_mongo_ids(stored)
 
+@router.post("/stored")
+async def new_item(item_data: Stored):
+    item = await db.stored.insert_one(item_data.dict())
+    return {"item_id": str(item.inserted_id)}
+
+
 # TODO: post new stored item, put more quantity
